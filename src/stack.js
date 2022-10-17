979a81bf4602672b5a85b1fff2b5754000d2913d
Andrey Lapitskiy <70910463+BBunny19@users.noreply.github.com>
2022-10-17 21:43:46 +0300

    Update stack.js

--- src/stack.js
+++ src/stack.js
@@ -1,31 +1,22 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Implement the Stack with a given interface via array.
- *
- * @example
- * const stack = new Stack();
- *
- * stack.push(1); // adds the element to the stack
- * stack.peek(); // returns the peek, but doesn't delete it, returns 1
- * stack.pop(); // returns the top element from stack and deletes it, returns 1
- * stack.pop(); // undefined
- *
- */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  _arr = []
+
+  push(element) {
+    this._arr = this._arr.concat(element)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this._arr.length == 0) return undefined
+    let res = this._arr[this._arr.length - 1]
+    this._arr = this._arr.slice(0, this._arr.length - 1)
+    return res
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this._arr.length == 0) return undefined
+    return this._arr[this._arr.length - 1]
   }
 }
 
